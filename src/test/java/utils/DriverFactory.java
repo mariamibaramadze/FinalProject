75413e6af5ba79056466a6399d7370cc783176a9
mariamibaramadze <mariamibaramadze@gmail.com>
2026-02-12 18:57:01 +0400

    finals

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -2,25 +2,29 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.time.Duration;
 
 public class DriverFactory {
-    private static WebDriver driver;
+
+    private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
-        return driver;
+        return driverThreadLocal.get();
     }
 
     public static void initDriver() {
-        driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driverThreadLocal.set(driver);
     }
 
     public static void quitDriver() {
+        WebDriver driver = driverThreadLocal.get();
         if (driver != null) {
             driver.quit();
-            driver = null;
+            driverThreadLocal.remove();
         }
     }
 }
